40144200e7f409bdc67ad568f2852eb120f4111a
alu0100698411 <alu0100698411@ull.edu.es>
2014-02-14 01:24:54 +0000

    Agregado caracteres especiales a test.js
    Al hacerlo con la template dentro del html y publicarlo coge los caracteres especiales \n(salto de linea) y \t (tab)

--- tests/tests.js
+++ tests/tests.js
@@ -5,14 +5,14 @@ suite('Suite CSV', function() {
    test('Creacion de tabla correcta (simple)', function () {
         original.value = '1,2,3\n4,5,6';
         calculate();
-        var valueExpected = '<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>1</td>                                  <td>2</td>                                  <td>3</td>              </tr>\n<tr>                    <td>4</td>                                  <td>5</td>                                  <td>6</td>              </tr>\n</tbody></table>';
+        var valueExpected = '<p>\n</p><table class="center" id="result">\n<tbody><tr>\n\t\t \n\t\t\t<td>1</td>\n\t\t \n\t\t\t<td>2</td>\n\t\t \n\t\t\t<td>3</td>\n\t\t \n\t</tr>\n<tr>\n\t\t \n\t\t\t<td>4</td>\n\t\t \n\t\t\t<td>5</td>\n\t\t \n\t\t\t<td>6</td>\n\t\t \n\t</tr>\n</tbody></table>';
         assert.deepEqual(finaltable.innerHTML, valueExpected);
     });
 	
 	test('Creacion de tabla correcta (compleja)', function () {
         original.value = '1,2,3,"4,5"\n6,7,8,"9,10"';
         calculate();
-        var valueExpected = '<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>1</td>                                  <td>2</td>                                  <td>3</td>                                  <td>4,5</td>              </tr>\n<tr>                    <td>6</td>                                  <td>7</td>                                  <td>8</td>                                  <td>9,10</td>              </tr>\n</tbody></table>';
+        var valueExpected = '<p>\n</p><table class="center" id="result">\n<tbody><tr>\n\t\t \n\t\t\t<td>1</td>\n\t\t \n\t\t\t<td>2</td>\n\t\t \n\t\t\t<td>3</td>\n\t\t \n\t\t\t<td>4,5</td>\n\t\t \n\t</tr>\n<tr>\n\t\t \n\t\t\t<td>6</td>\n\t\t \n\t\t\t<td>7</td>\n\t\t \n\t\t\t<td>8</td>\n\t\t \n\t\t\t<td>9,10</td>\n\t\t \n\t</tr>\n</tbody></table>';
         assert.deepEqual(finaltable.innerHTML, valueExpected);
     });
 	
@@ -24,7 +24,7 @@ suite('Suite CSV', function() {
    test('Creacion de tabla erronea', function () {
         original.value = '1,2,3,"4,5"\n6,7,8,"9,10",11';
         calculate();
-        var valueExpected = '<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>1</td>                                  <td>2</td>                                  <td>3</td>                                  <td>4,5</td>              </tr>\n<tr class="error">                    <td>6</td>                                  <td>7</td>                                  <td>8</td>                                  <td>9,10</td>                                  <td>11</td>              </tr>\n</tbody></table>';
+        var valueExpected = '<p>\n</p><table class="center" id="result">\n<tbody><tr>\n\t\t \n\t\t\t<td>1</td>\n\t\t \n\t\t\t<td>2</td>\n\t\t \n\t\t\t<td>3</td>\n\t\t \n\t\t\t<td>4,5</td>\n\t\t \n\t</tr>\n<tr class="error">\n\t\t \n\t\t\t<td>6</td>\n\t\t \n\t\t\t<td>7</td>\n\t\t \n\t\t\t<td>8</td>\n\t\t \n\t\t\t<td>9,10</td>\n\t\t \n\t\t\t<td>11</td>\n\t\t \n\t</tr>\n</tbody></table>';
         assert.deepEqual(finaltable.innerHTML, valueExpected);
     });
 	
