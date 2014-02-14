759901ef455dfe3ea0f71ce0a74c7ef32993c8a8
alu0100698411 <alu0100698411@ull.edu.es>
2014-02-14 01:18:15 +0000

    Suite CSV añadida

--- tests/tests.js
+++ tests/tests.js
@@ -1,34 +1,31 @@
 var assert = chai.assert;
 
-suite('Suite Temperaturas', function() {
-    test('32F = 0C', function() {
-        original.value = "32F";
+suite('Suite CSV', function() {
+
+   test('Creacion de tabla correcta (simple)', function () {
+        original.value = '1,2,3\n4,5,6';
         calculate();
-        assert.deepEqual(converted.innerHTML, "0.0 Celsius");
+        var valueExpected = '<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>1</td>                                  <td>2</td>                                  <td>3</td>              </tr>\n<tr>                    <td>4</td>                                  <td>5</td>                                  <td>6</td>              </tr>\n</tbody></table>';
+        assert.deepEqual(finaltable.innerHTML, valueExpected);
     });
-    test('45C = 113.0 Farenheit', function() {
-        original.value = "45C";
+	
+	test('Creacion de tabla correcta (compleja)', function () {
+        original.value = '1,2,3,"4,5"\n6,7,8,"9,10"';
         calculate();
-        assert.deepEqual(converted.innerHTML, "113.0 Farenheit");
+        var valueExpected = '<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>1</td>                                  <td>2</td>                                  <td>3</td>                                  <td>4,5</td>              </tr>\n<tr>                    <td>6</td>                                  <td>7</td>                                  <td>8</td>                                  <td>9,10</td>              </tr>\n</tbody></table>';
+        assert.deepEqual(finaltable.innerHTML, valueExpected);
     });
-    test('-98.572F = -72.5 Celsius', function() {
-        original.value = "-98.572F";
+	
+	test('Almacenamiento local', function () {
+        assert.deepEqual(localStorage.original, '1,2,3,"4,5"\n6,7,8,"9,10"');
+    });
+	
+	
+   test('Creacion de tabla erronea', function () {
+        original.value = '1,2,3,"4,5"\n6,7,8,"9,10",11';
         calculate();
-        assert.deepEqual(converted.innerHTML, "-72.5 Celsius");
+        var valueExpected = '<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>1</td>                                  <td>2</td>                                  <td>3</td>                                  <td>4,5</td>              </tr>\n<tr class="error">                    <td>6</td>                                  <td>7</td>                                  <td>8</td>                                  <td>9,10</td>                                  <td>11</td>              </tr>\n</tbody></table>';
+        assert.deepEqual(finaltable.innerHTML, valueExpected);
     });
-    test('529.35c = 984.8 Farenheit', function() {
-        original.value = "529.35c";
-        calculate();
-        assert.deepEqual(converted.innerHTML, "984.8 Farenheit");
-    });
-	test('458. = error', function() {
-        original.value = "458.";
-        calculate();
-        assert.match(converted.innerHTML, /ERROR/);
-    });
-    test('5x = error', function() {
-        original.value = "5x";
-        calculate();
-        assert.match(converted.innerHTML, /ERROR/);
-    });
+	
 });
